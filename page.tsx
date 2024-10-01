976184ab2c75e13701589dba8f306d92cee14e47
Ashar927 <160394814+Ashar927@users.noreply.github.com>
2024-10-02 00:04:01 +0500

    Update page.tsx

--- page.tsx
+++ page.tsx
@@ -6,7 +6,22 @@ export default function Home(){
   return(
     
 <div>
-  <h1>Home page</h1>
+ <br />
+  <h1> <b>HTML</b> </h1> <br />
+  <p>HTML stands for Hyper Text Markup Language. HTML is the standard markup language for creating Web pages. 
+    HTML describes the structure of a Web page. HTML consists of a series of elements. HTML elements tell the browser
+     how to display the content.</p> <br />
+ 
+  <h1> <b>CSS</b> </h1> <br />
+  <p>CSS is the acronym of “Cascading Style Sheets”. CSS is a computer language for laying out and structuring web pages (HTML or XML).
+     This language contains coding elements and is composed of these “cascading style sheets” which are equally called
+      CSS files (.</p> <br />
+
+  <h1> <b>Javascript</b> </h1> <br />
+        <p>JavaScript is a multi-paradigm, dynamic language with types and operators, standard built-in objects, and methods. Its syntax is based on the Java and C languages — many structures from those languages 
+          apply to JavaScript as well. JavaScript supports object-oriented programming with object
+           prototypes and classes.</p> 
+
   
 </div>
   )
